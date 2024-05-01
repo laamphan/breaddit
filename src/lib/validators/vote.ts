5a96a93764e288069a1c13ce4a4fba265806b055
laamphan <41721718+laamphan@users.noreply.github.com>
2024-05-01 17:51:14 +0700

    Fixing minor bugs

--- src/lib/validators/vote.ts
+++ src/lib/validators/vote.ts
@@ -7,9 +7,9 @@ export const PostVoteValidator = z.object({
 
 export type PostVoteRequest = z.infer<typeof PostVoteValidator>
 
-export const CommentVoteValidator = {
+export const CommentVoteValidator = z.object({
   postId: z.string(),
   voteType: z.enum(['UP', 'DOWN']),
-}
+})
 
 export type CommentVoteRequest = z.infer<typeof CommentVoteValidator>
