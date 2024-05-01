96ffc3e9493c1599af150910cfb80a75b86dc899
laamphan <41721718+laamphan@users.noreply.github.com>
2024-05-01 17:40:49 +0700

    Voting & Advanced caching for posts

--- src/components/PostFeed.tsx
+++ src/components/PostFeed.tsx
@@ -63,6 +63,8 @@ const PostFeed: FC<PostFeedProps> = ({ initialPosts, subredditName }) => {
           return (
             <li key={post.id} ref={ref}>
               <Post
+                currentVote={currentVote}
+                votesAmt={votesAmt}
                 commentAmt={post.comments.length}
                 post={post}
                 subredditName={post.subreddit.name}
@@ -73,6 +75,8 @@ const PostFeed: FC<PostFeedProps> = ({ initialPosts, subredditName }) => {
           return (
             <li key={post.id}>
               <Post
+                currentVote={currentVote}
+                votesAmt={votesAmt}
                 commentAmt={post.comments.length}
                 post={post}
                 subredditName={post.subreddit.name}
