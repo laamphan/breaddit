1c2ca7abccd660bee16d9a87bdbe3b5d7e31ec23
laamphan <41721718+laamphan@users.noreply.github.com>
2024-07-21 13:46:17 +0700

    Decrease build time by ignoring ts & eslint

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   images: {
     domains: ['uploadthing.com', 'lh3.googleusercontent.com', 'utfs.io' ],
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   experimental: {
     appDir: true,
   },
