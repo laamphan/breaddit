b76de4624b1012e7fdab518c71bbb90e78b267f5
laamphan <41721718+laamphan@users.noreply.github.com>
2024-07-21 19:48:59 +0700

    Update Shadcn components

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,4 @@
-import { ClassValue, clsx } from 'clsx'
+import { clsx, type ClassValue } from 'clsx'
 import { formatDistanceToNowStrict } from 'date-fns'
 import locale from 'date-fns/locale/en-US'
 import { twMerge } from 'tailwind-merge'
