15d84b519e5af79c98f7f142493a39f7847d6cb7
laamphan <41721718+laamphan@users.noreply.github.com>
2024-08-15 12:44:56 +0700

    update tiktok api

--- src/app/api/tiktok/callback/route.ts
+++ src/app/api/tiktok/callback/route.ts
@@ -10,15 +10,21 @@ export const GET = async (req: Request) => {
 
     const url = new URL(req.url)
     const code = url.searchParams.get('code')
+    if (!code) {
+      return new Response('Missing code parameter', { status: 400 })
+    }
+
+    const params = new URLSearchParams({
+      client_key: process.env.TIKTOK_CLIENT_KEY!,
+      client_secret: process.env.TIKTOK_CLIENT_SECRET!,
+      code,
+      grant_type: 'authorization_code',
+      redirect_uri: process.env.TIKTOK_REDIRECT_URI!,
+    })
+
     const response = await axios.post(
-      'https://open-api.tiktok.com/oauth/access_token/',
-      {
-        client_key: process.env.TIKTOK_CLIENT_KEY,
-        client_secret: process.env.TIKTOK_CLIENT_SECRET,
-        code,
-        grant_type: 'authorization_code',
-        redirect_uri: encodeURIComponent(process.env.TIKTOK_REDIRECT_URI!),
-      },
+      `https://open-api.tiktok.com/oauth/access_token/?${params.toString()}`,
+      null,
       {
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
@@ -26,13 +32,21 @@ export const GET = async (req: Request) => {
       }
     )
 
-    console.log('Tiktok response', response.data)
+    if (response.data.error) {
+      console.log('TikTok API error:', response.data)
+      return new Response(
+        `Error from TikTok: ${response.data.error_description}`,
+        { status: 400 }
+      )
+    }
+
+    console.log('TikTok response', response.data)
     return new Response('Success', { status: 200 })
 
     // Handle the response and save the access token
   } catch (error) {
     console.log('Error signing in with TikTok:', error)
-    return new Response('Error signing in with Tiktok', {
+    return new Response('Error signing in with TikTok', {
       status: 500,
     })
   }
